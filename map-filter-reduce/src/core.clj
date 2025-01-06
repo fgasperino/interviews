9810fb085c94837e4fe1a4ce2901c299a6c6dcea
Franco Gasperino <fgasperino@guaranteedrate.com>
2025-01-06 07:32:55 -0800

    Added project files

---
+++ map-filter-reduce/src/core.clj
@@ -0,0 +1,19 @@
+(ns core)
+
+(comment
+  ;; For the collection 'coll', perform the following:
+  ;;  1. increment every number in the sequence / vector by 1.
+  ;;  2. keep only numbers greater than, or equal to, 5.
+  ;;  3. summarize into a map tracking these 3 values:
+  ;;     3.1 the maximum number.
+  ;;     3.2 the minimum number.
+  ;;     3.3 the average of all numbers.
+
+  (def coll [0 1 2 3 4 5 6 7 8 9])
+
+  ;; Hints:
+  ;;   For 1. use the function 'map'
+  ;;   For 2. use the function 'filter'
+  ;;   For 3, use the function 'reduce.
+  )
+
